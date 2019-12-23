26b4dec2404214de32b107ea5945915bfc5aa318
Park Ju Hyung <qkrwngud825@gmail.com>
2019-12-23 18:31:37 +0900

    lmkd: make kernel trace logs optional
    Change-Id: I5628976440ebf68993e1f8e4829337f5cfc0f582
    Signed-off-by: Park Ju Hyung <qkrwngud825@gmail.com>
    Signed-off-by: HeroBuxx <herobuxx@gmail.com>

--- lmkd/lmkd.c
+++ lmkd/lmkd.c
@@ -88,6 +88,9 @@
 #define LINE_MAX 128
 #define MAX_NR_ZONES 6
 
+// #define ENABLE_KTRACE_LOGS
+#define TRACE_MARKER_PATH "/sys/kernel/debug/tracing/trace_marker"
+
 /* Android Logger event logtags (see event.logtags) */
 #define MEMINFO_LOG_TAG 10195355
 
@@ -1385,6 +1388,7 @@ static int parse_one_zone_watermark(char *buf, struct watermark_info *w)
     return ret;
 }
 
+#ifdef ENABLE_KTRACE_LOGS
 static void trace_log(char *fmt, ...)
 {
     char buf[PAGE_SIZE];
@@ -1421,6 +1425,9 @@ static void trace_log(char *fmt, ...)
 		ALOG##X(fmt);  \
 		trace_log(fmt); \
 		})
+#else
+#define ULMK_LOG(X, fmt...) ALOG##X(fmt)
+#endif
 static int file_cache_to_adj(int nr_file)
 {
     int min_score_adj = OOM_SCORE_ADJ_MAX + 1;

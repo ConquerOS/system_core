324caffe994d86a804db7e096c546233db5a9d49
Pulkit077 <pulkitagarwal2k1@gmail.com>
2024-01-14 08:22:16 +0530

    DNM

--- init/property_service.cpp
+++ init/property_service.cpp
@@ -1314,7 +1314,7 @@ void PropertyLoadBootDefaults() {
     update_sys_usb_config();
 
     // Workaround SafetyNet
-    workaround_snet_properties();
+//    workaround_snet_properties();
 }
 
 bool LoadPropertyInfoFromFile(const std::string& filename,
